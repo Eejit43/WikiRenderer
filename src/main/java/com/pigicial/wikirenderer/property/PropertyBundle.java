d3ebd33085c55d6be99504d030038aaa73142bbe
Pigicial <72773248+Pigicial@users.noreply.github.com>
2026-06-04 23:21:02 -0700

    sub-folder support (will do macos and linux testing for this in a bit)

--- src/main/java/com/pigicial/wikirenderer/property/PropertyBundle.java
+++ src/main/java/com/pigicial/wikirenderer/property/PropertyBundle.java
@@ -93,7 +93,7 @@ public interface PropertyBundle {
 
     default void buildFileNameGUIControls(Renderable<?> renderable, RenderScreen screen, FlowLayout container) {
         screen.fileNameField = WikiRendererUI.labelledTextField(container, renderable.getCustomFileName(), "file_name", Sizing.expand(90));
-        screen.fileNameField.setFilter(s -> s.matches("^[^<>:\"/\\\\|?*\\x00-\\x1F]*$")); // file name regex
+        screen.fileNameField.setFilter(s -> s.matches("^[^<>:\"|?*\\\\x00-\\\\1F]*$")); // file name regex
         screen.fileNameField.onChanged().subscribe(renderable::setCustomFileName);
     }
 
--- src/main/java/com/pigicial/wikirenderer/render/batch/BatchPropertyBundle.java
+++ src/main/java/com/pigicial/wikirenderer/render/batch/BatchPropertyBundle.java
@@ -154,7 +154,7 @@ public class BatchPropertyBundle extends DefaultCroppablePropertyBundle {
         BatchRenderable<?> batchRenderable = (BatchRenderable<?>) renderable;
         if (!batchRenderable.delegates.isEmpty() && this.batchRenderable.currentDelegate instanceof DynamicBatchLabelProvider labelProvider) {
             screen.fileNameField = WikiRendererUI.labelledTextField(container, fileNameFormatter, "batch.file_name_preset", Sizing.fixed(120));
-            screen.fileNameField.setFilter(s -> s.matches("^[^<>:\"/\\\\|?*\\x00-\\x1F]*$")); // file name regex
+            screen.fileNameField.setFilter(s -> s.matches("^[^<>:\"|?*\\\\x00-\\\\1F]*$")); // file name regex
             screen.fileNameField.onChanged().subscribe(renderable::setCustomFileName);
 
             WikiRendererUI.text(container, "batch.label_presets", 10);
