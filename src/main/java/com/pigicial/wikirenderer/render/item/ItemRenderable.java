1a854b56d9e73a5a968635aba6c6f8ecd7adf760
Pigicial <72773248+Pigicial@users.noreply.github.com>
2026-07-12 18:31:33 -0700

    make timing text respect model overrides

--- src/main/java/com/pigicial/wikirenderer/render/item/ItemRenderable.java
+++ src/main/java/com/pigicial/wikirenderer/render/item/ItemRenderable.java
@@ -64,6 +64,10 @@ public class ItemRenderable extends ItemBasedRenderable<ItemRenderablePropertyBu
         return currentModelIndex;
     }
 
+    public @Nullable List<ItemModel> getModels() {
+        return models;
+    }
+
     @Override
     public String buildFileName(String preset) {
         String id = BuiltInRegistries.ITEM.getKey(this.stack.getItem()).getPath();
@@ -160,7 +164,7 @@ public class ItemRenderable extends ItemBasedRenderable<ItemRenderablePropertyBu
     @Override
     public List<List<Integer>> getTicksToFullyAnimate() {
         List<Integer> animationTimings = new LinkedList<>();
-        AnimationTimingUtil.scanTicksToFullyAnimateItem(stack, animationTimings);
+        AnimationTimingUtil.scanTicksToFullyAnimateItem(this, animationTimings);
         return List.of(animationTimings);
     }
 }
